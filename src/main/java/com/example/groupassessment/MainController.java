aa8eeed911d7e6534f3264789cf70bc6693ddf7a
TheOostman <ethan.oosthuiz3n@gmail.com>
2024-09-14 01:17:13 +1000

    updating UI disign - Ethan.O

--- src/main/java/com/example/groupassessment/MainController.java
+++ src/main/java/com/example/groupassessment/MainController.java
@@ -1,14 +1,22 @@
 package com.example.groupassessment;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 public class MainController {
     @FXML
     private Label welcomeText;
+    @FXML
+    private Button profileButton;
 
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+    }
+
+    @FXML
+    protected void viewProfileClick() {
+        profileButton.setText("NO");
     }
 }
