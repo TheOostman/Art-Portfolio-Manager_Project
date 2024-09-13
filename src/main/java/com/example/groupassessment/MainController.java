836e756661bbfc050940bdd50711dcbf41d75b33
TheOostman <ethan.oosthuiz3n@gmail.com>
2024-09-14 01:41:44 +1000

    setup for the start of login/register - Ethan.O

--- src/main/java/com/example/groupassessment/MainController.java
+++ src/main/java/com/example/groupassessment/MainController.java
@@ -3,20 +3,29 @@ package com.example.groupassessment;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class MainController {
     @FXML
     private Label welcomeText;
     @FXML
     private Button profileButton;
-
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
+    private Button signinRegister;
+    private TextField usernameEntry;
+    private TextField passwordEntry;
+
+
 
     @FXML
     protected void viewProfileClick() {
-        profileButton.setText("NO");
+        profileButton.setText("SIGNIN FIRST");
     }
+
+    @FXML
+    protected void signInRegisterBn() {
+        System.out.println(usernameEntry);
+        System.out.println(passwordEntry);
+    }
+
 }
