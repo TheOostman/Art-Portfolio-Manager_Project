70a92483bd30552ee526cc95633693ce39792f4d
TheOostman <ethan.oosthuiz3n@gmail.com>
2024-09-13 20:59:34 +1000

    updating UI disign

--- src/main/java/com/example/groupassessment/MainApplication.java
+++ src/main/java/com/example/groupassessment/MainApplication.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class MainApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("mainWindowControls.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1200, 800);
         stage.setTitle("Art Portfolio Manager");
         stage.setScene(scene);
