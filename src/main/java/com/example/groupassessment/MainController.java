5f6e58a204cfc6696b8d3e8dc8e0bffa0718349f
TheOostman <ethan.oosthuiz3n@gmail.com>
2024-09-17 16:14:04 +1000

    default testing data  - Ethan

--- src/main/java/com/example/groupassessment/MainController.java
+++ src/main/java/com/example/groupassessment/MainController.java
@@ -2,14 +2,13 @@ package com.example.groupassessment;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
 public class MainController {
     @FXML
     private Button profileButton;
-    @FXML
-    private Button signinRegister;
+    //@FXML
+    //private Button signinRegister;
     @FXML
     private TextField usernameEntry;
     @FXML
@@ -17,11 +16,14 @@ public class MainController {
 
 
     public static boolean isLoggedIn = true;        // Please use this boolean for signing and register !!!!
+    String basicUsername = "username1";             //Test data, this can be deleted
+    String basicPassword = "123";                   //Test data, this can be deleted
 
     @FXML
     protected void viewProfileClick() {
-        if (isLoggedIn == true){
-            profileButton.setText("asd");
+
+        if (isLoggedIn){
+            System.out.println(basicUsername);
         }
         else{
             profileButton.setText("SIGNIN FIRST");
