9c35057e3ac95587290968a3fdcc78843b288643
TheOostman <ethan.oosthuiz3n@gmail.com>
2024-09-17 16:54:28 +1000

    Updated Design  - Ethan

--- src/main/java/com/example/groupassessment/MainController.java
+++ src/main/java/com/example/groupassessment/MainController.java
@@ -2,6 +2,7 @@ package com.example.groupassessment;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
 public class MainController {
